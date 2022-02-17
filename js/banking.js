146914bf26fb0479e84c53cd320bd1f91c3fb68e
Rasujon3 <rasujon3@gmail.com>
2022-02-17 23:53:17 +0600

    30_5-1 Revisit Baap er bank with Clean functional approach

--- js/banking.js
+++ js/banking.js
@@ -2,11 +2,22 @@ function getInputValue(fieldId) {
   const inputField = document.getElementById(fieldId);
   const valueInText = inputField.value;
   const value = parseFloat(valueInText);
+  inputField.value = "";
   return value;
+}
+
+function updateTotal(fieldId, amount) {
+  const totalTag = document.getElementById(fieldId);
+  const previousTotalInText = totalTag.innerText;
+  const previousTotal = parseFloat(previousTotalInText);
+  const newTotal = previousTotal + amount;
+  totalTag.innerText = newTotal;
 }
 
 document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
-    console.log("hi");
+    const amount = getInputValue("deposit-input");
+    console.log(amount);
+    updateTotal("deposit-total", amount);
   });
